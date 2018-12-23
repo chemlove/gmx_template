1ff97e6e5d2fa61883b24d7824d42a6be76f2b0c
Alexey Shaytan <alexsha@newton.bioeng.ru>
2018-12-23 18:48:25 +0300

    Update

---
+++ get_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+case "$1" in
+        1)
+curl -s https://bb06d1eb07ea8543d3ce493b455852a7d0b1b7f1@raw.githubusercontent.com/intbio/gmx_template/master/1_prepare_system.ipynb -o 1_prepare_system.ipynb 
+            ;;
+         
+        2)
+curl -s https://bb06d1eb07ea8543d3ce493b455852a7d0b1b7f1@raw.githubusercontent.com/intbio/gmx_template/master/2_min_equil.ipynb -o 2_min_equil.ipynb
+
+
+            ;;
+        3)
+curl -s https://bb06d1eb07ea8543d3ce493b455852a7d0b1b7f1@raw.githubusercontent.com/intbio/gmx_template/master/3_benchmarking.ipynb -o 3_benchmarking.ipynb
+
+
+            ;;
+        4)
+curl -s https://bb06d1eb07ea8543d3ce493b455852a7d0b1b7f1@raw.githubusercontent.com/intbio/gmx_template/master/4_production_run.ipynb -o 4_production_run.ipynb
+
+
+            ;;
+        5)
+curl -s https://bb06d1eb07ea8543d3ce493b455852a7d0b1b7f1@raw.githubusercontent.com/intbio/gmx_template/master/5_trj_preprocessing.ipynb -o 5_trj_preprocessing.ipynb
+
+
+            ;;
+        6)
+curl -s https://bb06d1eb07ea8543d3ce493b455852a7d0b1b7f1@raw.githubusercontent.com/intbio/gmx_template/master/6_1_basic_analysis.ipynb -o 6_1_basic_analysis.ipynb
+
+
+            ;;
+ 
+esac
