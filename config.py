5c0db9dc3e81330255256784be24e6b3696cf001
Alexey Shaytan <alexsha@newton.bioeng.ru>
2018-12-23 19:20:35 +0300

    Update

--- config.py
+++ config.py
@@ -2,4 +2,9 @@ movtitle="MD simulations of H3-H4, tails truncated"
 folder="h3-h4_tm" #unique folder name for simulations on supercomputer
 scuser="shaytan_1609" #username on lomonosoc
 pname=folder #project name
-descr="MD simulations of H3-H4, tails truncated, AMBER14SB force field"
+descr="""
+# MD simulations of H3-H4, tails truncated
+- AMBER14SB force field
+- 50 mM NaCl
+- box 2nm
+"""
