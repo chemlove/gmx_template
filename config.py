3b6329a780479468eb0f1a6fef2746de3b97ddaa
Alexey Shaytan <alexsha@newton.bioeng.ru>
2019-01-13 02:19:35 +0300

    upd

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-movtitle="MD simulations of H3-H4, tails truncated"
-folder="h3-h4_tm" #unique folder name for simulations on supercomputer
-scuser="shaytan_1609" #username on lomonosoc
+movtitle="MD simulations of H3-H4 with DNA, tails truncated"
+folder="h3-h4_tm_DNA" #unique folder name for simulations on supercomputer
 pname=folder #project name
 ssh_host="lomo2" #The name of entry in ssh config file to connect to a supercomputer
 descr="""
-# MD simulations of H3-H4, tails truncated
+# MD simulations of H3-H4 with 30 bp of DNA, tails truncated
 - AMBER14SB force field
 - 50 mM NaCl
 - box 2nm
