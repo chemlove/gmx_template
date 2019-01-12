61abc5a47b95375d2b7e01c283f798ad67487fd9
Alexey Shaytan <alexsha@newton.bioeng.ru>
2019-01-13 00:38:11 +0300

    upd

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ movtitle="MD simulations of H3-H4, tails truncated"
 folder="h3-h4_tm" #unique folder name for simulations on supercomputer
 scuser="shaytan_1609" #username on lomonosoc
 pname=folder #project name
+ssh_host="lomo2" #The name of entry in ssh config file to connect to a supercomputer
 descr="""
 # MD simulations of H3-H4, tails truncated
 - AMBER14SB force field
