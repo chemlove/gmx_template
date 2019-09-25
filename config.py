226df3e0450d69dc12f22ea33b1d5e2cc36ba8d0
Alexey Shaytan <alexsha@newton.bioeng.ru>
2019-09-26 01:51:37 +0300

    Update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 movtitle="MD simulations of H3-H4 with DNA, tails truncated"
-folder="h3-h4_tm_DNA" #unique folder name for simulations on supercomputer
+folder="h3-h4_tm_30DNA" #unique folder name for simulations on supercomputer
 pname=folder #project name
 ssh_host="lomo2g" #The name of entry in ssh config file to connect to a supercomputer
 descr="""
